48fcfbe7ca07851dbd6ce63f545c0bcfa2baac24
Justin Cheung <justin.cheung@healthoutcomesww.com>
2021-05-20 13:57:52 -0300

    Dummy data insertion script for Clients

---
+++ a1/ClientG4.sql
@@ -0,0 +1,3 @@
+INSERT INTO `ClientG4` (`clientCompIdG4`, `clientCompName`, `clientCityG4`, `clientCompPasswordG4`, `moneyOwedG4`) VALUES ('1', 'Ford', 'Halifax', '12345', '1000');
+INSERT INTO `ClientG4` (`clientCompIdG4`, `clientCompName`, `clientCityG4`, `clientCompPasswordG4`, `moneyOwedG4`) VALUES ('2', 'Toyota', 'Toronto', 'abc123', '3.50');
+INSERT INTO `ClientG4` (`clientCompIdG4`, `clientCompName`, `clientCityG4`, `clientCompPasswordG4`, `moneyOwedG4`) VALUES ('3', 'Nissan', 'Vancouver', 'nitro', '100.10');

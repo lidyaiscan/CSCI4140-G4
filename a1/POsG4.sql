3fe8a65db1af95cd1c76af3b2ec72f5902fa8aa9
chloe bae <chloe.bae@dal.ca>
2021-05-20 02:13:12 -0300

    Updated Time in POsG4 Data

--- a1/POsG4.sql
+++ a1/POsG4.sql
@@ -1,10 +1,10 @@
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(1, 2, '2021-05-11 02:08:38', 1);
-INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(2, 1, '2020-07-31 16:20:31', 5);
+INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(2, 1, '2020-07-31 11:20:31', 5);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(3, 3, '2021-04-23 09:25:54', 4);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(4, 4, '2021-01-09 01:09:11', 3);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(5, 5, '2020-11-28 06:10:45', 5);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(6, 6, '2021-04-19 11:14:00', 1);
-INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(7, 7, '2021-03-22 12:04:08', 2);
+INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(7, 7, '2021-03-22 09:04:08', 2);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(8, 8, '2020-12-02 03:53:16', 4);
 INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(9, 9, '2019-05-20 01:10:24', 5);
-INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(10, 10, '2021-05-09 12:15:01', 1);
+INSERT INTO `POsG4` (`poNoG4`, `clientCompIdG4`, `datePOG4`, `statusG4`) VALUES(10, 10, '2021-05-09 10:15:01', 1);

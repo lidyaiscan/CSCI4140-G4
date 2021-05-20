20fd8112fa489c9eb68bb808d6058614647b308b
Justin Cheung <justin.cheung@healthoutcomesww.com>
2021-05-20 18:12:02 -0300

    Remove extraneous files

--- a1/StatusG4.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO StatusG4 (statusDescriptionG4) VALUES ('Prepared for shipping');
-INSERT INTO StatusG4 (statusDescriptionG4) VALUES ('On the way');
-INSERT INTO StatusG4 (statusDescriptionG4) VALUES ('Receiving');
-INSERT INTO StatusG4 (statusDescriptionG4) VALUES ('Received');
-INSERT INTO StatusG4 (statusDescriptionG4) VALUES ('Canceled order');

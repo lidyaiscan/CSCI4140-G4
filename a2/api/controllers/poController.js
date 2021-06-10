bd27afc4bc1b25b37b4903dcd51c9a2448dee4ab
Justin Cheung <justin.cheung@healthoutcomesww.com>
2021-06-10 17:56:27 -0300

    Fix clientGetPoByNoG4 to properly use id parameter

--- a2/api/controllers/poController.js
+++ a2/api/controllers/poController.js
@@ -54,6 +54,10 @@ const getPoByNoG4 = (req, res) => new Promise((resolve, reject) => {
 // Return all Purchase Orders By clientCompId
 const clientGetPosG4 = (req, res) => new Promise((resolve, reject) => {
     const clientCompIdG4 = req.params.clientCompIdG4
+    
+    if (isNaN(clientCompIdG4)) {
+        return res.status(400).send('Bad Request - clientCompIdG4 must be a number') // Return a 400 - Bad Request
+    }
 
     const q = `SELECT POsG4.poNoG4, POsG4.clientCompIdG4, statusG4, clientCompNameG4, datePOG4, statusDescriptionG4, SUM(POLinesG4.linePriceG4) AS TOTAL_COST, SUM(POLinesG4.qtyG4) as TOTAL_QTY FROM POsG4 
     INNER JOIN statusG4 ON POsG4.statusG4=StatusG4.statusNoG4 
@@ -80,8 +84,13 @@ const clientGetPosG4 = (req, res) => new Promise((resolve, reject) => {
 // Return specific Purchase Order by poNo and clientCompId
 const clientGetPoByNoG4 = (req, res) => new Promise((resolve, reject) => {
     const poNoG4 = req.params.poNoG4
+    const clientCompIdG4 = req.params.clientCompIdG4
 
-    // Check if the parameter provided is a valid number
+    // Check if the parameters provided are valid numbers
+    if (isNaN(clientCompIdG4)) {
+        return res.status(400).send('Bad Request - clientCompIdG4 must be a number') // Return a 400 - Bad Request
+    }
+
     if (isNaN(poNoG4)) {
         return res.status(400).send('Bad Request - poNoG4 must be a number') // Return a 400 - Bad Request
     }
@@ -90,7 +99,7 @@ const clientGetPoByNoG4 = (req, res) => new Promise((resolve, reject) => {
     INNER JOIN statusG4 ON POsG4.statusG4=StatusG4.statusNoG4 
     INNER JOIN ClientG4 ON POsG4.clientCompIdG4=ClientG4.clientCompIdG4 
     INNER JOIN POLinesG4 ON POsG4.poNoG4=POLinesG4.poNoG4
-    WHERE POLinesG4.poNoG4 = ${poNoG4}`;
+    WHERE POLinesG4.poNoG4 = ${poNoG4} AND POsG4.clientCompIdG4 = ${clientCompIdG4}`;
     const db = conn.getDB();
     db.query(q, (err, data) => {
 

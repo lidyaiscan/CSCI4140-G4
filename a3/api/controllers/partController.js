f6578f746f91eb190a430f97c556ffffead46aa7
chloe bae <chloe.bae@dal.ca>
2021-06-23 17:35:46 -0300

    Added SP PROC_PART_PRICE_ADJUSTMENT_G4 and added updatePartPriceByNoG4()

--- a3/api/controllers/partController.js
+++ a3/api/controllers/partController.js
@@ -71,8 +71,34 @@ const updatePartByNoG4 = (req, res) => new Promise((resolve, reject) => {
     });
 });
 
+// Update the PRICE ONLY for a specific Part specified as the partNoG4
+const updatePartPriceByNoG4 = (req, res) => new Promise((resolve, reject) => {
+
+    const body = req && req.body;
+    const params = req && req.params;
+
+    // Check if the provided parameter is a valid number.
+    if (isNaN(params.partNoG4) || isNaN(body.currentPriceG4) ||
+        params.partNoG4 < 0 || body.currentPriceG4 < 0) {
+        return res.status(400).send('Bad Request - partNoG4 and currentPriceG4 must be positive numbers') // Return a 400 - Bad Request
+    }
+
+    // Call a stored procedure to process the update transaction.
+    const q = `call PROC_PART_PRICE_ADJUSTMENT_G4(${params.partNoG4}, ${body.currentPriceG4});`;
+    const db = conn.getDB();
+    db.query(q,  (err, data) => {
+
+        // Error occured with request
+        if (err) return reject(err);
+
+        // Send the process results to the requestor.
+        return res.send(data);
+    });
+});
+
 module.exports = {    
     getPartsG4,
     getPartByNoG4,
-    updatePartByNoG4
+    updatePartByNoG4,
+    updatePartPriceByNoG4
 }
--- a3/api/routes/index.js
+++ a3/api/routes/index.js
@@ -6,6 +6,7 @@ const partController = require('../controllers/partController');
 const clientController = require('../controllers/clientController');
 const paymentController = require('../controllers/paymentController');
 
+//** Shared Service **//
 router.get('/pos', poController.getPosG4);
 router.get('/pos/:poNoG4', poController.getPoByNoG4);
 router.get('/parts', partController.getPartsG4);
@@ -13,16 +14,16 @@ router.get('/parts/:partNoG4', partController.getPartByNoG4);
 router.get('/clients', clientController.getClientsG4);
 router.get('/clients/:clientCompIdG4', clientController.getClientByNoG4);
 
-// Client API
+//** Client-specific Service **//
 router.get('/client/pos/:clientCompIdG4', poController.clientGetPosG4);
 router.get('/client/pos/:clientCompIdG4/:poNoG4', poController.clientGetPoByNoG4);
+// additional feature
+router.patch('/client/payment/:clientCompIdG4/:poNoG4', paymentController.makePaymentByPoNoAndClientCompIdG4);
 
-// Client Agent service vii. List POs; viii. Track One PO
+//** Agent-specific Service **//
 router.get('/agent/pos', poController.getPosG4);
 router.get('/agent/pos/:poNoG4', poController.getPoByNoG4);
-
-// Additional Features
-router.patch('/client/payment/:clientCompIdG4/:poNoG4', paymentController.makePaymentByPoNoAndClientCompIdG4);
 router.patch('/agent/parts/:partNoG4', partController.updatePartByNoG4);
+router.patch('/agent/parts/:partNoG4/price', partController.updatePartPriceByNoG4);
 
 module.exports = router;
